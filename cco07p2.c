db116e8daaa994917f379a5a2198d0812b5f6702
mady20 <165700212+mady20@users.noreply.github.com>
2025-07-27 23:47:48 +0530

    optimization next

--- cco07p2.c
+++ cco07p2.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+int identical_right(int snow1[], int snow2[], int start){
+  int offset;
+  for(offset = 0; offset < 6; offset++){
+    if(snow1[offset] != snow2[ (start + offset) % 6] ) {
+      return 0;
+    };
+  }
+  return 1;
+}
+
+int identical_left(int snow1[], int snow2[], int start){
+  int offset, snow2_index;
+  for(offset = 0; offset < 6; offset++){
+    snow2_index = start - offset;
+    if (snow2_index <= -1) snow2_index += 6;
+    if (snow1[offset] != snow2[snow2_index]) return 0;
+  }
+  return 1;
+}
+
+int are_identical(int snow1[], int snow2[]){
+  int start;
+  for(start = 0; start < 6; start++){
+    if(identical_right(snow1, snow2, start))  return 1;
+    if(identical_left(snow1, snow2, start))   return 1;
+  }
+  return 0;
+}
 
 void identify_identical(int values[], int  n){ 
   int i, j;
@@ -14,9 +42,28 @@ void identify_identical(int values[], int  n){
   printf("No two integers are alike.\n");
 }
 
+void solve(int snowflakes[][],  int n){
+  int i, j;
+  for(i = 0; i < n; i++){
+    for (j = i + 1; j < n; j++){
+      if(are_identical(snowflakes[i], snowflakes[j])){
+        printf("Twin snowflakes found.");
+      }
+    }
+  }
+  print("No two snowflakes are alike.");
+}
 
 int main(void){
-  int values[] = {1, 2, 3, 1, 5};
-  identify_identical(values, 5);
+  int n, i, j;
+  int snowflakes[n][6];
+  scanf("%d", &n);
+  for(i = 0; i < n; i++){
+    for(j = 0; j < 6; j++){
+     scanf("%d", &snowflakes[i][j]);
+    }
+  }
+
+  solve(snowflakes, n);
   return 0;
 }
