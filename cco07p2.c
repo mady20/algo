394c4e560ca5ea1a7af244548387d310e04bd4a8
mady20 <165700212+mady20@users.noreply.github.com>
2025-07-29 00:03:15 +0530

    new approach

--- cco07p2.c
+++ cco07p2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+
+#define SIZE 100000
+
+typedef struct snowflake_node {
+  int snowflake[6];
+  struct snowflake_node *next;
+} snowflake_node;
+
+int code(int snowflake[]){
+  return (snowflake[0] + snowflake[1] + snowflake[2] + snowflake[3]
+      + snowflake[4] + snowflake[5]) % SIZE;
+}
+
 int identical_right(int snow1[], int snow2[], int start){
   int offset;
   for(offset = 0; offset < 6; offset++){
@@ -29,41 +42,37 @@ int are_identical(int snow1[], int snow2[]){
   return 0;
 }
 
-void identify_identical(int values[], int  n){ 
-  int i, j;
-  for(i = 0; i < n; i++){
-    for(j = i + 1; j < n; j++){
-      if(values[i] == values[j]){
-        printf("Twin integers found.\n");
-        return;
-      }
-    }
-  }
-  printf("No two integers are alike.\n");
-}
-
-void solve(int snowflakes[][],  int n){
+// TODO: https://c-faq.com/aryptr/pass2dary.html
+void solve(int snowflakes[][6],  int n){
   int i, j;
   for(i = 0; i < n; i++){
     for (j = i + 1; j < n; j++){
       if(are_identical(snowflakes[i], snowflakes[j])){
-        printf("Twin snowflakes found.");
+        printf("Twin snowflakes found.\n");
+        return;
       }
     }
   }
-  print("No two snowflakes are alike.");
+  printf("No two snowflakes are alike.\n");
 }
 
 int main(void){
-  int n, i, j;
-  int snowflakes[n][6];
+  static snowflake_node *snowflakes[SIZE] = {NULL};
+  snowflake_node *snow;
+  int n, i, j, snowflake_code;
   scanf("%d", &n);
   for(i = 0; i < n; i++){
-    for(j = 0; j < 6; j++){
-     scanf("%d", &snowflakes[i][j]);
+    snow = malloc(sizeof(snowflake_node));
+    if (snow == NULL) {
+      fprintf(stderr, "malloc error!\n");
+      exit(1);
     }
+    for(j = 0; j < 6; j++){
+      scanf("%d", &snow->snowflake[j]);
+    }
+    snowflake_code = code(snow->snowflake);
+    snow->next = snowflakes[snowflake_code];
+    snowflakes[snowflake_code] = snow;
   }
-
-  solve(snowflakes, n);
   return 0;
 }
